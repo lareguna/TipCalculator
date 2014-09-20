b6b183c293acf49ca1af6c216634d0eec8883471
Lakshmi <lakshmi.reguna@yahoo.com>
2014-09-19 23:20:20 -0700

    Corrected the Bill Amount field and ensured that Tip Percentage is correctly calculated

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tipLabel: UILabel!
+   
     
     @IBOutlet weak var billField: UITextField!
     
     @IBOutlet weak var totalLabel: UILabel!
-    
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
@@ -23,7 +22,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
+        
+
         totalLabel.text = "$0.00"
     }
 
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         var tipPercentages = [0.18, 0.2, 0.22]
@@ -42,7 +41,7 @@ class ViewController: UIViewController {
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
-        tipLabel.text = String(format: "%.2f", tip)
+  
         totalLabel.text = String(format: "%.2f", total)
     }
 
